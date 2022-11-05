968b8ab5cd6a2f630418edcb23b5707d9a476c80
Grigory Kovalenko <mail@kogri.ru>
2022-11-05 14:17:45 +0000

    compass WIP

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
-/* const output1 = document.getElementById('output1');
-navigator.geolocation.watchPosition(pos => {
-    const result = {
-      accuracy: pos.coords.accuracy,
-      altitude: pos.coords.altitude,
-      altitudeAccuracy: pos.coords.altitudeAccuracy,
-      heading: pos.coords.heading,
-      latitude: pos.coords.latitude,
-      longitude: pos.coords.longitude,
-      speed: pos.coords.speed,
-    };
-    output1.textContent = JSON.stringify(result, null, 2);
-});*/
+/*
+navigator.geolocation.watchPosition(pos => console.log({
+    accuracy: pos.coords.accuracy,
+    altitude: pos.coords.altitude,
+    altitudeAccuracy: pos.coords.altitudeAccuracy,
+    heading: pos.coords.heading,
+    latitude: pos.coords.latitude,
+    longitude: pos.coords.longitude,
+    speed: pos.coords.speed,
+}));
+*/
 
-const output = document.querySelector('.output');
 const compass = document.querySelector('.compass');
 const arrow = document.querySelector('.compass__arrow');
+
 /**
  * @param {Event} e
  */
 function handler(e) {
   const alpha = typeof e.webkitCompassHeading === 'undefined' ?
     e.alpha : Math.abs(e.webkitCompassHeading - 360);
-  const result = {
-    alpha: alpha && alpha.toFixed(),
-    beta: alpha && e.beta.toFixed(),
-  };
-  output.textContent = JSON.stringify(result, null, 2);
-  if (result.alpha) {
-    arrow.style.transform = `rotateZ(${result.alpha}deg)`;
+  if (alpha) {
+    arrow.style.transform = `rotateZ(${alpha}deg)`;
   }
-  if (result.beta) {
-    compass.style.transform = `rotateX(${result.beta}deg)`;
+  if (e.beta) {
+    compass.style.transform = `rotateX(${e.beta}deg)`;
   }
 }
+
 if (typeof window.ondeviceorientationabsolute !== 'undefined') {
   window.addEventListener('deviceorientationabsolute', handler, true);
 } else if (typeof window.ondeviceorientation !== 'undefined') {
