976b231ee48212133267eb2afe94c56c760d2da4
Grigory Kovalenko <mail@kogri.ru>
2022-11-05 13:49:18 +0000

    add compas draft

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*const output1 = document.getElementById('output1');
+/* const output1 = document.getElementById('output1');
 navigator.geolocation.watchPosition(pos => {
     const result = {
       accuracy: pos.coords.accuracy,
@@ -13,16 +13,28 @@ navigator.geolocation.watchPosition(pos => {
 });*/
 
 const output = document.querySelector('.output');
+const compass = document.querySelector('.compass');
+const arrow = document.querySelector('.compass__arrow');
+/**
+ * @param {Event} e
+ */
 function handler(e) {
-    const alpha = typeof e.webkitCompassHeading === 'undefined' ? e.alpha : Math.abs(e.webkitCompassHeading - 360);
-    const result = {
-        alpha: alpha && alpha.toFixed(),
-        beta: alpha && e.beta.toFixed(),
-    };
-    output.textContent = JSON.stringify(result, null, 2);
+  const alpha = typeof e.webkitCompassHeading === 'undefined' ?
+    e.alpha : Math.abs(e.webkitCompassHeading - 360);
+  const result = {
+    alpha: alpha && alpha.toFixed(),
+    beta: alpha && e.beta.toFixed(),
+  };
+  output.textContent = JSON.stringify(result, null, 2);
+  if (alpha) {
+    arrow.style.transform = `rotateZ($(alpha)deg)`;
+  }
+  if (beta) {
+    compass.style.transform = `rotateX($(beta)deg)`;
+  }
 }
 if (typeof window.ondeviceorientationabsolute !== 'undefined') {
-    window.addEventListener("deviceorientationabsolute", handler, true);
+  window.addEventListener('deviceorientationabsolute', handler, true);
 } else if (typeof window.ondeviceorientation !== 'undefined') {
-    window.addEventListener("deviceorientation", handler, true);
+  window.addEventListener('deviceorientation', handler, true);
 }
